a482fd8f5509601e9830d75f5fb0ba1df9d53d10
Ramon Antonio Parada <ramon@bigpress.net>
2011-06-18 11:56:54 +0200

    first commit

--- setup.sh
+++ setup.sh
@@ -8,3 +8,11 @@
  rm ~/.vimrc
  ln -s $DOTPATH/vimrc ~/.vimrc
 
+
+git config --global user.name "Ramon Antonio Parada"
+git config --global user.email "ramon@bigpress.net"
+
+echo Remember to setup github token
+git config --global github.user brainsqueezer
+#git config --global github.token 0123456789yourf0123456789token
+git config --global core.excludesfile ~/.gitignore
